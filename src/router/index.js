fc165d77022c060002eb2f5d53578c4109a1d9ab
zll <2023305043@qq.com>
2022-11-29 16:48:35 +0800

    云e办--登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import Login from '@/views/Login'
+import Home from '@/views/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+const proxyObj = {}
+
+proxyObj['/'] = {
+  // websocket
+  ws: false,
+  // 目标地址
+  target: 'http://localhost:8081',
+  // 发送请求头host会被设置target
+  changeOrigin: true,
+  // 不重写请求地址
+  pathReWrite: {
+    '^/': '/'
+  }
+}
+
+// proxyObj['/ws'] = {
+//   ws: true,
+//   target: 'ws://localhost:8989'
+// }
+
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: proxyObj
+
+  }
+}
